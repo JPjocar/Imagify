59e318bd067fd59d2de53939fb4ff6fb02e0f5c4
JPJocar <jpjocar.6@gmail.com>
2024-10-14 09:55:35 -0500

    Base del proyecto

---
+++ src/main/java/com/example/Imagify/ImagifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Imagify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Imagify/ImagifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Imagify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
