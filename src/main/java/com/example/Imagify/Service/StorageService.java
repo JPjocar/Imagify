ea99ef172c7760eb0820af18bdf622aa870de312
JPJocar <jpjocar.6@gmail.com>
2024-10-16 14:42:35 -0500

    mostrar y subir imagenes

---
+++ src/main/java/com/example/Imagify/Service/StorageService.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Imagify.Service;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Dev
+ */
+@Service
+public class StorageService {
+    private final Path rootLocation = Paths.get("images");
+    public Resource loadResource(String filename){
+        try{
+            Path file = rootLocation.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if(resource.exists() || resource.isReadable()){
+                return resource;
+            }else{
+                throw new RuntimeException("No se puede leer el archivo");
+            }
+        }catch(MalformedURLException e){
+            throw new RuntimeException("No se encuentra el archivo");
+        }
+    }
+}
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,33 @@
+// const open = document.getElementById('open');
+const gallery = document.querySelector(".gallery");
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+gallery.addEventListener('click', mostrarImagen);
+
+
+function mostrarImagen(e){
+  if(e.target.classList.contains("item-img")){
+    leeDatos(e.target.nextElementSibling);  
+    modal_container.classList.add('show'); 
+  }
+}
+
+function leeDatos(imagenDetalles){
+  const imgInfo = {
+    titulo: imagenDetalles.querySelector("h1").textContent,
+    descripcion: imagenDetalles.querySelector("p").textContent,
+    src: imagenDetalles.previousElementSibling.src
+  }
+  imagenHTML(imgInfo);
+}
+
+function imagenHTML(img){
+  modal_container.querySelector(".title").textContent = img.titulo;
+  modal_container.querySelector(".description").textContent = img.descripcion;
+  modal_container.querySelector(".show-img").src = img.src;
+}
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
